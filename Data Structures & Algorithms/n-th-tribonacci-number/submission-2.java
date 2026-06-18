543e793dca4c17aab542442dbb86e3f8f8d2630c
Krithik Adav <112746235+adav-07@users.noreply.github.com>
2026-06-18 11:30:25 +0530

    Add: n-th-tribonacci-number - submission-2

---
+++ Data Structures & Algorithms/n-th-tribonacci-number/submission-2.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int tribonacci(int n) {
+        if(n<2){
+            return n;
+        }
+        int a=0, b=1, c=1, d=1;
+        for(int i=3;i<=n;i++){
+            d=a+b+c;
+            a=b;
+            b=c;
+            c=d;
+        }
+        return d;
+    }
+}
