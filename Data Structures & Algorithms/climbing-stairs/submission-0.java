c95fe3e7f3d53daee5e7f82852cfa5937221ebb8
Krithik Adav <112746235+adav-07@users.noreply.github.com>
2026-06-18 11:11:25 +0530

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int climbStairs(int n) {
+        int a=1, b=2, c=n;
+        for(int i=2;i<n;i++){
+            c=a+b;
+            a=b;
+            b=c;
+        }
+        return c;
+    }
+}
